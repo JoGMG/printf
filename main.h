c326f3df89792754e66d918d1b883bf4cc69c3cb
Joshua Attah <112636188+JoGMG@users.noreply.github.com>
2022-11-22 23:24:50 +0100

    main.h

--- main.h
+++ main.h
@@ -7,23 +7,44 @@
 #include <unistd.h>
 
 /**
- * struct symfunc - defines a structure for symbols and functions.
- * @sym: The operator.
- * @f: The function associated.
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
 **/
-struct symfunc
+struct flags
 {
-	char *sym;
-	int (*f)(va_list);
-};
-typedef struct symfunc sym_func;
+	int plus;
+	int space;
+	int hash;
+}
+typedef struct flags flags_t;
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+**/
+struct printHandler
+{
+	char c
+	int (*f)(va_list ap, flags_t *f)
+}
+typedef struct ph;
+
 
 /* Functions */
 int _printf(const char *format, ...);
-int parser(const char *format, sym_func f_list[], va_list arg_list);
-int _write_char(char);
-int print_char(va_list);
-int print_string(va_list);
-int print_percent(va_list);
+
+int _putchar(char c);
+int _puts(char *str);
+
+int (*get_print(char s))(va_list, flags_t *);
+
+int print_char(va_list l, flags_t *f);
+int print_string(va_list l, flags_t *f);
+int print_percent(va_list l, flags_t *f);
 
 #endif
