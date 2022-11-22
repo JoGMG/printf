fb20a4714bfe9d2a243af67a64769341186df870
Joshua Attah <112636188+JoGMG@users.noreply.github.com>
2022-11-22 23:45:48 +0100

    all for printf

---
+++ select_print.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * select_print - selects the right printing function
+ * depending on the conversion specifier passed to _printf
+ * @s: character that holds the conversion specifier
+ * Description: the function loops through the structs array
+ * spec_func[] to find a match between the specifier passed to _printf
+ * and the first element of the struct, and then the approriate
+ * printing function
+ * Return: a pointer to the matching printing function
+ */
+int (*get_print(char s))(va_list, flags_t *)
+{
+	ph spec_func[] = {
+		{'i', print_int},
+		{'s', print_string},
+		{'c', print_char},
+		{'d', print_int},
+		{'u', print_unsigned},
+		{'x', print_hex},
+		{'X', print_hex_big},
+		{'b', print_binary},
+		{'o', print_octal},
+		{'R', print_rot13},
+		{'r', print_rev},
+		{'S', print_bigS},
+		{'p', print_address},
+		{'%', print_percent}
+		};
+	int count = 14;
+
+	register int i;
+
+	for (i = 0; i < count; i++)
+		if (spec_func[i].c == s)
+			return (spec_func[i].f);
+	return (NULL);
+}
