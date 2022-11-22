58c5c9699936181194b086b469f7e0d1d51a0d01
Joshua Attah <112636188+JoGMG@users.noreply.github.com>
2022-11-22 23:37:14 +0100

    all for printf

--- format_specifiers.c
+++ format_specifiers.c
@@ -34,3 +34,16 @@ int print_char(va_list l, flags_t *f)
     _putchar(va_arg(l, int));
     return (1);
 }
+
+/**
+ * print_percent - prints a percent
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags in which we turn the flags on
+ * Return: number of char printed
+ */
+int print_percent(va_list l, flags_t *f)
+{
+	(void)l;
+	(void)f;
+	return (_putchar('%'));
+}
